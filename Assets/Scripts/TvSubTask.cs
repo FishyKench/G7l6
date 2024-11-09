46b157e05a22df81db2b703a0d105327eedf29c2
FishyKench <m7md.al7male@gmail.com>
2024-11-09 07:51:30 +0300

    TvSubTask Logically done :)

--- Assets/Scripts/TvSubTask.cs
+++ Assets/Scripts/TvSubTask.cs
@@ -10,39 +10,46 @@ public class TvSubTask : TaskBase, IInteractable
 
     private bool isAddingStress = true;
 
+    [SerializeField]
+    private AudioSource TvStatic;
+
     protected override void Start()
     {
         base.Start();
         stressManager = FindObjectOfType<StressManager>();
-        StartCoroutine(AddStressPeriodically(1, 0.6f));
     }
 
     protected override void TaskFailed()
     {
-        //task can not be failed xd
+        // This task cannot fail :)
+    }
+
+    public override void StartTask()
+    {
+        base.StartTask();
+        StartCoroutine(AddStressPeriodically(1, 0.6f)); // Start coroutine to add stress over time
+        TvStatic.Play(); 
     }
 
     protected override void RewardPlayer()
     {
-        // Remove half stress as a reward.
         if (stressManager != null)
         {
-            stressManager.RemoveStress(10);
+            stressManager.RemoveStress(10); // Reduce stress as a reward
         }
-
-        Debug.Log("Player rewarded for completing dummy task!");
+        Debug.Log("Player rewarded for completing TV task!");
         gameManager.CompleteTask(this);
     }
 
     private IEnumerator AddStressPeriodically(float interval, float stressPoints)
     {
-        while (isAddingStress)
+            TvStatic.Play();
+        while (isAddingStress && isTaskActive)
         {
             yield return new WaitForSeconds(interval);
-            if (isTaskActive && stressManager != null)
+            if (stressManager != null)
             {
-                stressManager.AddStress(stressPoints);
-                print("momo called");
+                stressManager.AddStress(stressPoints); // Add stress periodically
             }
         }
     }
@@ -50,17 +57,18 @@ public class TvSubTask : TaskBase, IInteractable
     private void StopAddingStress()
     {
         isAddingStress = false;
-        StopAllCoroutines(); 
+        StopAllCoroutines();
+        TvStatic.Stop();
     }
 
     public void Interact()
     {
         if (isTaskActive)
         {
-            gameManager.CompleteTask(this);
-            gameManager.taskUIManager.UpdateTaskSlots(gameManager.taskQueue);
             StopAddingStress();
             RewardPlayer();
+            gameManager.CompleteTask(this);
+            gameManager.taskUIManager.UpdateTaskSlots(gameManager.taskQueue);
         }
     }
 }
